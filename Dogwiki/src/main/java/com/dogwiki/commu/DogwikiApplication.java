cd5b94fa368911c2b494c11db2ef5f9bb64377ad
yeonsu10 <jenix1@naver.com>
2023-03-27 16:55:13 +0900

    feat : 프로젝트 시작 설정

---
+++ Dogwiki/src/main/java/com/dogwiki/commu/DogwikiApplication.java
@@ -0,0 +1,13 @@
+package com.dogwiki.commu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogwikiApplication.class, args);
+	}
+
+}
---
+++ Dogwiki/src/test/java/com/dogwiki/commu/DogwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogwiki.commu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
