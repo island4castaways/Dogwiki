63a8f6cb258610aa4302bbc4424713ee1473ec23
monaYR <rufinayk@naver.com>
2023-04-12 18:02:33 +0900

    fix: join에서 중복 이메일 검사 추가

--- Dogwiki/src/main/java/com/dogwiki/commu/UserController.java
+++ Dogwiki/src/main/java/com/dogwiki/commu/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
 
 	@GetMapping("/login")
 	public String user_login_get(HttpSession session) {
-		if(session.getAttribute("userid") != null) {
+		if (session.getAttribute("userid") != null) {
 			return "/user/mypage";
 		}
 		return "/user/login";
@@ -31,15 +31,16 @@ public class UserController {
 
 	// 로그인
 	@PostMapping("/login")
-	public String user_login_post(@RequestParam("userid") String userid, 
-			@RequestParam("pw") String pw,
+	public String user_login_post(@RequestParam("userid") String userid, @RequestParam("pw") String pw,
 			HttpSession session, Model model, RedirectAttributes rttr) {
+
 		try {
-			if(userService.validateId(userid)) {
-				UserEntity entity = userService.validate(userid);
+			if (userService.validateId(userid)) {
+				UserEntity entity = userService.selectById(userid);
 				if(entity.getPw().equals(pw)) {
 					session.setAttribute("userid", entity.getUserid());
 					session.setAttribute("username", entity.getUsername());
+
 					return "user/mypage";
 				}
 			}
@@ -52,7 +53,7 @@ public class UserController {
 
 	@GetMapping("/join")
 	public String user_join_get(HttpSession session) {
-		if(session.getAttribute("userid") != null) {
+		if (session.getAttribute("userid") != null) {
 			return "/user/mypage";
 		}
 		return "/user/join";
@@ -67,29 +68,28 @@ public class UserController {
 			@RequestParam("email") String email, 
 			HttpSession session, Model model, 
 			RedirectAttributes rttr) {
-		// id,email 가 사용중인지 확인 후 값들을 받아서 entity생성 한 후 session을 가지고 return
-		try {
-			if(userService.validateId(userid)) {
-				throw new RuntimeException("Invalide argument.");
-			} else {
-				UserEntity entity = UserEntity.builder().userid(userid).pw(pw).username(username).phone(phone)
-						.email(email).build();
-				userService.create(entity);
-
-				rttr.addFlashAttribute("msg", "회원가입을 성공했습니다.");
-				return "redirect:/login";
-			}
-		} catch (Exception e) {
-			rttr.addFlashAttribute("errorMessage", "유효하지 않은 형식입니다.");
+		//id, email 사용중인지 확인 후 값들을 받아서 entity생성 한 후 session을 가지고 return
+		if(userService.validateId(userid)) {
+			rttr.addFlashAttribute("msg", "이미 사용 중인 아이디입니다.");
 			return "redirect:/join";
-		}
+		} else if(userService.validateEmail(email)) {
+			rttr.addFlashAttribute("msg", "이미 사용 중인 이메일입니다.");
+			return "redirect:/join";
+		} else {
+			UserEntity entity = UserEntity.builder().userid(userid).pw(pw).username(username).phone(phone)
+					.email(email).build();
+			userService.create(entity);
 
+			// rttr.addFlashAttribute("msg", "회원가입을 성공했습니다.");
+			rttr.addFlashAttribute("msg", "회원가입을 성공했습니다.");
+			return "redirect:/login";
+		}
 	}
 
 	// my page
 	@GetMapping("/mypage")
 	public String mypage_get(HttpSession session) {
-		if(session.getAttribute("userid") == null) {
+		if (session.getAttribute("userid") == null) {
 			return "/user/login";
 		}
 		return "/user/mypage";
@@ -97,7 +97,7 @@ public class UserController {
 
 	@PostMapping("/mypage")
 	public String mypage(HttpSession session) {
-		if(session.getAttribute("userid") == null) {
+		if (session.getAttribute("userid") == null) {
 			return "/user/login";
 		}
 		return "/user/mypage";
@@ -107,7 +107,7 @@ public class UserController {
 	// 현재 비밀번호가 맞을 경우에 변경비밀번호로 업데이트
 	@GetMapping("/user_change_pw")
 	public String user_change_pw(HttpSession session) {
-		if(session.getAttribute("userid") == null) {
+		if (session.getAttribute("userid") == null) {
 			return "/user/login";
 		}
 		return "/user/user_change_pw";
@@ -115,45 +115,42 @@ public class UserController {
 
 	@PostMapping("/user_change_pw")
 	public String user_change_pw_ok( // 마이페이지로 이동
-			HttpSession session, @RequestParam("newpw") String newpw, 
-			@RequestParam("oldpw") String oldpw,
-			Model model, RedirectAttributes rttr) {
+			HttpSession session, @RequestParam("newpw") String newpw, @RequestParam("oldpw") String oldpw, Model model,
+			RedirectAttributes rttr) {
 		String userid = (String) session.getAttribute("userid");
-		if(userid == null) {
+		if (userid == null) {
 			return "/user/login";
 		}
 
 		Boolean result = userService.pwmodify(userid, oldpw, newpw);
-		if(result == true) { // 로그인 성공 : 1) 세션을 생성(id, name) 2)mypage로 이동
+		if (result == true) { // 로그인 성공 : 1) 세션을 생성(id, name) 2)mypage로 이동
 			rttr.addFlashAttribute("msg", "비밀번호 변경을 완료했습니다.");
 			return "redirect:/mypage";
 		} else {
 			rttr.addFlashAttribute("msg", "비밀번호 변경을 실패했습니다.");
 			return "redirect:/mypage";
 		}
-
 	}
 
 	// 회원 탈퇴
 	@GetMapping("/user_delete")
 	public String user_delete(HttpSession session) {
-		if(session.getAttribute("userid") == null) {
+		if (session.getAttribute("userid") == null) {
 			return "/user/login";
 		}
 		return "/user/user_delete";
 	}
 
 	@PostMapping("/user_delete")
-	public String user_delete_check(@RequestParam("pw") String pw, 
-			HttpSession session, Model model,
+	public String user_delete_check(@RequestParam("pw") String pw, HttpSession session, Model model,
 			RedirectAttributes rttr) {
 		String userid = (String) session.getAttribute("userid");
-		if(userid == null) {
+		if (userid == null) {
 			return "/user/login";
 		}
-		
+
 		Boolean result = userService.deleteUser(userid, pw);
-		if(result) {
+		if (result) {
 			rttr.addFlashAttribute("msg", "회원 삭제를 성공했습니다.");
 			session.invalidate();
 			return "redirect:/login";
@@ -161,19 +158,17 @@ public class UserController {
 			rttr.addFlashAttribute("msg", "비밀번호가 맞지 않습니다.");
 			return "redirect:/user_delete";
 		}
-
 	}
 
 	// 정보 수정
 	@GetMapping("/user_update")
 	public String user_update( // 마이페이지로 이동
 			HttpSession session, Model model) {
-		if(session.getAttribute("userid") == null) {
+		if (session.getAttribute("userid") == null) {
 			return "/user/login";
 		}
-		
 		String userid = (String) session.getAttribute("userid");
-		UserEntity entity = userService.validate(userid);
+		UserEntity entity = userService.selectById(userid);
 
 		model.addAttribute("entity", entity);
 		return "/user/user_update";
@@ -181,17 +176,11 @@ public class UserController {
 
 	@PostMapping("/user_update")
 	public String user_update_ok( // 마이페이지로 이동
-			@RequestParam("username") String username, 
-			@RequestParam("phone") String phone,
-			@RequestParam("email") String email, 
-			HttpSession session, Model model, 
-			RedirectAttributes rttr) {
+			HttpSession session, @RequestParam("username") String username, @RequestParam("phone") String phone,
+			@RequestParam("email") String email, Model model, RedirectAttributes rttr) {
 		String userid = (String) session.getAttribute("userid");
-
 		Boolean result = userService.modify(userid, username, phone, email);
-		model.addAttribute("result", result); // model에 entity정보 넘김
-
-		if(result == true) {
+		if (result == true) {
 			session.setAttribute("username", username);
 			rttr.addFlashAttribute("msg", "회원 수정을 완료했습니다.");
 			return "redirect:/mypage";
@@ -202,7 +191,7 @@ public class UserController {
 
 	@GetMapping("/user_logout")
 	public String logout(HttpSession session, Model model, RedirectAttributes rttr) {
-		if(session.getAttribute("userid") == null) {
+		if (session.getAttribute("userid") == null) {
 			return "/user/login";
 		}
 		session.removeAttribute("userid"); // 특정 세션 삭제
